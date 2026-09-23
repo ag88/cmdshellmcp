5191dbe03a00250ae3948b7cabca5bfca5ed696d
ag88 <ag88@github.com>
2026-09-23 15:37:07 +0800

    refactor hardcoded allowed list to be simply ['date']
    refactor hardcoded allowed list to be simply ['date']
    the rest are commented and left in code as examples

--- cmdshellmcp2.py
+++ cmdshellmcp2.py
@@ -63,24 +63,17 @@ configure_audit()
 # Define your allowed whitelist
 SAFE_COMMANDS = ["date"]
 
-BASIC_COMMANDS = ["ls", "pwd", "echo", "whoami"]
-# more
-MORE_COMMANDS = ["cat", "head", "tail", "grep", "egrep", "sed", "wc", "file", "tree",
-    "du", "df", "free", "ps", "uname", "hostname", "uptime", "w", "last"]
-# file change commands
-UPD_COMMANDS = ["mkdir", "cp", "mv", "awk"] 
+##BASIC_COMMANDS = ["ls", "pwd", "echo", "whoami"]
+### more
+##MORE_COMMANDS = ["cat", "head", "tail", "grep", "egrep", "sed", "wc", "file", "tree",
+##    "du", "df", "free", "ps", "uname", "hostname", "uptime", "w", "last"]
+### file change commands
+##UPD_COMMANDS = ["mkdir", "cp", "mv", "awk"] 
 
 DEFAULT_ALLOWED_COMMANDS = SAFE_COMMANDS
-DEFAULT_ALLOWED_COMMANDS.extend(BASIC_COMMANDS)
-DEFAULT_ALLOWED_COMMANDS.extend(MORE_COMMANDS)
-DEFAULT_ALLOWED_COMMANDS.extend(UPD_COMMANDS)
-
-##DEFAULT_ALLOWED_COMMANDS = [
-##    "ls", "pwd", "echo", "date", "whoami",
-##    "cat", "head", "tail", "grep", "egrep", "sed", "wc", "file", "du",
-##    "df", "free", "ps", "uname", "hostname", "uptime", "w", "last",
-##    "mkdir", "cp", "mv", "awk",
-##]
+##DEFAULT_ALLOWED_COMMANDS.extend(BASIC_COMMANDS)
+##DEFAULT_ALLOWED_COMMANDS.extend(MORE_COMMANDS)
+##DEFAULT_ALLOWED_COMMANDS.extend(UPD_COMMANDS)
 
 # Replaced with the effective configuration before tools are registered.
 ALLOWED_COMMANDS = DEFAULT_ALLOWED_COMMANDS.copy()
